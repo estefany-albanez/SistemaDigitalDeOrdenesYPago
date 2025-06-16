b3a5190b5e80661f750f59c830361872aa9db830
Karla Menendez <mr17054@ues.edu.sv>
2025-06-15 22:04:56 -0600

    Agregando ruta /dashboard para DashboardPage.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,18 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'promotions', component: () => import('pages/PromotionsPage.vue') }
-    ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+{path: '/',
+component: () => import('layouts/MainLayout.vue'),
+children: [
+{ path: '', component: () => import('pages/ProductsPage.vue') },
+{ path: 'dashboard', component: () => import('pages/DashboardPage.vue') },
+{ path: 'order-modification', component: () => import('pages/OrderModificationPage.vue')
+},
+{ path: 'payment', component: () => import('pages/PaymentPage.vue') },
+{ path: 'contact', component: () => import('pages/ContactPage.vue') } // Añade esta línea
 ]
-
+},
+{
+path: '/:catchAll(.)',
+component: () => import('pages/ErrorNotFound.vue')
+}
+] 
 export default routes
